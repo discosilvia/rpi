471d2892e39ba3d44eeb692c1e393d043c67a1d3
discosilvia <discosilvia@gmail.com>
2018-05-26 19:47:06 +0200

    cleaned up code from rpi

--- spiralgame.py
+++ spiralgame.py
@@ -1,3 +1,4 @@
+# last version tested on rpi
 # This is Python 3 code
 #import libraries
 import RPi.GPIO as GPIO
@@ -18,27 +19,27 @@ GPIO.setup(16,GPIO.IN) # button
 print ("Press the button to begin!")
 
 #Specify folder of MP3s
-soundfiles = glob.glob(“/home/pi/Desktop/*.mp3”)
+soundfiles = glob.glob("/home/pi/Desktop/*.mp3")
 pygame.mixer.music.load(random.choice(soundfiles)) #moved this out of play funtion
 
 #play function
 def play():
-	if pygame.mixer.music.get_busy() == true:
-	print("unpausing mp3")
-	#this might not work :/
-	pygame.mixer.music.unpause()
-		elif 
-	print("first time playing mp3")
-	#music repeats indefinitely
-	pygame.mixer.music.play(-1)
+        if pygame.mixer.music.get_busy() == true:
+                print("unpausing mp3")
+                #this might not work :/
+                pygame.mixer.music.unpause()
+        else: 
+                print("first time playing mp3")
+                #music repeats indefinitely
+                pygame.mixer.music.play(-1)
 
 def pause():
-	print(“mp3 paused”)
-	pygame.mixer.music.pause()
+        print("mp3 paused")
+        pygame.mixer.music.pause()
 
 #empty while loop that exits when button pressed
 while GPIO.input(16)==0:
-    pass #Pass is a null operation
+        pass #Pass is a null operation
 
 print ("Game is live")
 
@@ -47,16 +48,16 @@ time.sleep(0.5)
 
 #while loop until button pressed
 while GPIO.input(16) == 0:
-    if GPIO.input(7) == 1:
-        #turn on relay      
-        GPIO.output(18,True)
-        # turn on sound
-        play()
-    elif GPIO.input(7) == 0:
-        # turn off relay
-        GPIO.output(18,False)
-        # turn off sound
-        pause()   
+        if GPIO.input(7) == 1:
+                #turn on relay      
+                GPIO.output(18,True)
+                # turn on sound
+                play()
+        elif GPIO.input(7) == 0:
+                # turn off relay
+                GPIO.output(18,False)
+                # turn off sound
+                pause()   
 
 print ("Game is off")
 
