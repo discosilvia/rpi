0f9d03fa44ff5d5bc3f9a93e2503899f7f97a664
discosilvia <discosilvia@gmail.com>
2018-05-27 10:50:44 +0200

    todo list update + pin 22

--- spiralgame.py
+++ spiralgame.py
@@ -1,4 +1,4 @@
-# last version tested on rpi
+# 27/5 10:40
 # This is Python 3 code
 #import libraries
 import RPi.GPIO as GPIO
@@ -12,11 +12,11 @@ pygame.mixer.init()
 
 #set GPIO numbering mode and define input and output pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18,GPIO.OUT) #relay
+GPIO.setup(22,GPIO.OUT) #relay - don't use pin 18, apperently cannot be combined with audio
 GPIO.setup(7,GPIO.IN) #wire and loop
 GPIO.setup(16,GPIO.IN) # button
 
-print ("Press the button to begin!")
+print ("tap start to begin!")
 
 #Specify folder of MP3s
 soundfiles = glob.glob("/home/pi/Desktop/*.mp3")
@@ -41,7 +41,7 @@ def pause():
 while GPIO.input(16)==0:
         pass #Pass is a null operation
 
-print ("Game is live")
+print ("live")
 
 #wait for debounce before beginning
 time.sleep(0.5)
@@ -50,16 +50,16 @@ time.sleep(0.5)
 while GPIO.input(16) == 0:
         if GPIO.input(7) == 1:
                 #turn on relay      
-                GPIO.output(18,True)
+                GPIO.output(22,False) #relay is on when low
                 # turn on sound
                 play()
         elif GPIO.input(7) == 0:
                 # turn off relay
-                GPIO.output(18,False)
+                GPIO.output(22,True) #relay is on when high
                 # turn off sound
                 pause()   
 
-print ("Game is off")
+print ("off")
 
 #cleanup the GPIO pins before ending
 GPIO.cleanup()
