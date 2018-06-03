6dad17889f139e4436beebdfb892047758f1571e
discosilvia <discosilvia@gmail.com>
2018-06-03 12:53:23 +0000

    infinite loop & all working

--- spiralgame.py
+++ spiralgame.py
@@ -1,14 +1,13 @@
-# 27/5 10:40
+# 3/6
 # This is Python 3 code
 #import libraries
 import RPi.GPIO as GPIO
 import time
 import pygame.mixer
-import glob
-import random
 
 #Fire up the sound mixer
-pygame.mixer.init()
+pygame.mixer.init(22100, -16, 2, 512)
+#gets rid of delay
 
 #set GPIO numbering mode and define input and output pins
 GPIO.setmode(GPIO.BOARD)
@@ -16,50 +15,61 @@ GPIO.setup(22,GPIO.OUT) #relay - don't use pin 18, apperently cannot be combined
 GPIO.setup(7,GPIO.IN) #wire and loop
 GPIO.setup(16,GPIO.IN) # button
 
-print ("tap start to begin!")
+try:
+        #Specify MP3
+        pygame.mixer.music.load("/home/pi/Desktop/example.mp3")
+        print("loaded mp3")
 
-#Specify folder of MP3s
-soundfiles = glob.glob("/home/pi/Desktop/*.mp3")
-pygame.mixer.music.load(random.choice(soundfiles)) #moved this out of play funtion
+        while (1 == 1):
+                print ("tap start to begin!")
+        
+                #play function
+                def play():
+                        if pygame.mixer.music.get_busy() == True:
+                                #print("unpausing mp3")
+                                pygame.mixer.music.unpause()
+                        else: 
+                                #print("first time playing mp3")
+                                #music repeats indefinitely
+                                pygame.mixer.music.play(-1)
 
-#play function
-def play():
-        if pygame.mixer.music.get_busy() == true:
-                print("unpausing mp3")
-                #this might not work :/
-                pygame.mixer.music.unpause()
-        else: 
-                print("first time playing mp3")
-                #music repeats indefinitely
-                pygame.mixer.music.play(-1)
+                def pause():
+                        pygame.mixer.music.pause()
 
-def pause():
-        print("mp3 paused")
-        pygame.mixer.music.pause()
+                #empty while loop that exits when button pressed
+                while GPIO.input(16)==0:
+                        pass #Pass is a null operation
 
-#empty while loop that exits when button pressed
-while GPIO.input(16)==0:
-        pass #Pass is a null operation
-
-print ("live")
-
-#wait for debounce before beginning
-time.sleep(0.5)
-
-#while loop until button pressed
-while GPIO.input(16) == 0:
-        if GPIO.input(7) == 1:
-                #turn on relay      
+                print ("live")
+                #flicker to start      
                 GPIO.output(22,False) #relay is on when low
-                # turn on sound
-                play()
-        elif GPIO.input(7) == 0:
-                # turn off relay
-                GPIO.output(22,True) #relay is on when high
-                # turn off sound
-                pause()   
+                time.sleep(0.2)
+                GPIO.output(22,True) #relay is on when low
+                time.sleep(0.2)
+                GPIO.output(22,False) #relay is on when low
+                time.sleep(0.2)
+                GPIO.output(22,True) #relay is on when low
 
-print ("off")
+                #while loop until button pressed
+                while GPIO.input(16) == 0:
+                        if GPIO.input(7) == 1:
+                                # turn on sound
+                                play()
+                                #turn on relay      
+                                GPIO.output(22,False) #relay is on when low                        
+                        elif GPIO.input(7) == 0:
+                                # turn off sound
+                                pause()
+                                # turn off relay
+                                GPIO.output(22,True) #relay is off when high
+                           
+
+                print ("off")
 
 #cleanup the GPIO pins before ending
-GPIO.cleanup()
+except KeyboardInterrupt:
+        print ("ctrl c")
+except:
+        print ("other error")
+finally:
+        GPIO.cleanup()
