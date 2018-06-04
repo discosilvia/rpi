2d2f5611b30929e55bcea85907e0258d0c236957
discosilvia <discosilvia@gmail.com>
2018-06-04 09:48:43 +0000

    4/6 fixed button and added visual cue for off

--- spiralgame.py
+++ spiralgame.py
@@ -19,57 +19,68 @@ try:
         #Specify MP3
         pygame.mixer.music.load("/home/pi/Desktop/example.mp3")
         print("loaded mp3")
+        print ("tap start to begin!")
+                       
+        #play function
+        def play():
+                if pygame.mixer.music.get_busy() == True:
+                        #print("unpausing mp3")
+                        pygame.mixer.music.unpause()
+                else: 
+                        #print("first time playing mp3")
+                        #music repeats indefinitely
+                        pygame.mixer.music.play(-1)
 
-        while (1 == 1):
-                print ("tap start to begin!")
-        
-                #play function
-                def play():
-                        if pygame.mixer.music.get_busy() == True:
-                                #print("unpausing mp3")
-                                pygame.mixer.music.unpause()
-                        else: 
-                                #print("first time playing mp3")
-                                #music repeats indefinitely
-                                pygame.mixer.music.play(-1)
+        def pause():
+                pygame.mixer.music.pause()
 
-                def pause():
-                        pygame.mixer.music.pause()
-
+        run = 0
+        while True :
                 #empty while loop that exits when button pressed
-                while GPIO.input(16) == 0:
-                        pass #Pass is a null operation
-
-                print ("live")
-                #flicker to start      
-                #GPIO.output(22,True) #relay is on when low
-                #time.sleep(0.2)
-                #GPIO.output(22,False) #relay is on when low
-                #time.sleep(0.2)
-                #GPIO.output(22,True) #relay is on when low
-                #time.sleep(0.2)
-                #GPIO.output(22,False) #relay is on when low
-
-                #while loop until button pressed
-                while GPIO.input(16) == 0:
-                        if GPIO.input(7) == 1:
-                                # turn on sound
-                                play()
-                                #turn on relay      
-                                GPIO.output(22,True)                         
-                        elif GPIO.input(7) == 0:
-                                # turn off sound
-                                pause()
-                                # turn off relay
-                                GPIO.output(22,False)
-                           
-
-                print ("off")
+                #while GPIO.input(16) == 0:
+                #       pass #Pass is a null operation
+                if GPIO.input(16) == 0 and run == 0:
+                        print ("live")                              
+                        while GPIO.input(16) == 0:
+                                if GPIO.input(7) == 1:
+                                        # turn on sound
+                                        play()
+                                        print ("play")
+                                        #turn on relay      
+                                        GPIO.output(22,True)                         
+                                elif GPIO.input(7) == 0:
+                                        # turn off sound
+                                        pause()
+                                        print ("pause")
+                                        # turn off relay
+                                        GPIO.output(22,False)
+                        
+                        
+                                time.sleep(0.1)
+                        run = 1
+                
+                if GPIO.input(16) == 0 and run == 1:
+                        print ("off")
+                        GPIO.output(22,False)
+                        time.sleep(0.2)
+                        GPIO.output(22,True)
+                        time.sleep(0.2)
+                        GPIO.output(22,False)
+                        time.sleep(0.2)
+                        GPIO.output(22,True)
+                        time.sleep(0.2)
+                        GPIO.output(22,False)
+                                                                        
+                        run = 0
+                        while GPIO.input(16) == 0:
+                                time.sleep(0.1)
 
 #cleanup the GPIO pins before ending
 except KeyboardInterrupt:
         print ("ctrl c")
+        GPIO.cleanup()
 except:
         print ("other error")
+        GPIO.cleanup()
 finally:
         GPIO.cleanup()
